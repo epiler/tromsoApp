dd1ea668403a73f7cd1c6b6febd1c0ee9d76256f
207 <207@academicos.cesde.edu.co>
2023-07-04 20:46:34 -0500

    se crea menu de opciones

--- app/src/main/java/com/example/turistiandov2/Home.java
+++ app/src/main/java/com/example/turistiandov2/Home.java
@@ -3,6 +3,8 @@ package com.example.turistiandov2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -31,5 +33,33 @@ public class Home extends AppCompatActivity {
             }
         });
 
+
+
+
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemSeleccionado=item.getItemId();
+        switch (itemSeleccionado){
+
+            case(R.id.opcion1):
+                break;
+            case(R.id.opcion2):
+                break;
+            case(R.id.opcion3):
+                break;
+            case(R.id.opcion4):
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
